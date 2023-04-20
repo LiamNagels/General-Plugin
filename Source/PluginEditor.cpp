f0d0ac9d3d8ae774e8081f36d33c2e351415dbbb
LiamNagels <Liam.Nagels@Student.Howest.be>
2023-04-20 09:17:56 +0200

    Revert "Push test"
    This reverts commit 120e39ccbabd86ca8109091ed01e23e198f4a987.

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,7 +30,7 @@ void GeneralPluginAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello ISP!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void GeneralPluginAudioProcessorEditor::resized()
